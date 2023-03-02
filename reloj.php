a06e3a465841285eaf93cba7e3c1ee82a4562634
Cristhian Paucara <ivan.paucara@uab.edu.bo>
2023-03-02 16:47:32 -0400

    creacion db usuarios

--- reloj.php
+++ reloj.php
@@ -1,13 +1,17 @@
 <?php
     class BaseDatos extends SQLite3{
-        function_contruct(){
-            $this->open('usuarios.db');
+        function __construct()
+        {
+            $this->open("usuarios.db");
         }
     }
+
     $db = new BaseDatos();
     if($db){
         echo"<p> La base de datos usuarios fue abierta exitosamente</p>";
     }else{
         echo"<p>EROOR al abrir la base de datos usuarios</p>";
     }
+    //en caso de error por sqlite3 revisar este link para corregir
+    //https://parzibyte.me/blog/2018/02/07/habilitar-extension-sqlite3-php/
 ?>
