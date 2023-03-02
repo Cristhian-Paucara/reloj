ecb8f0101017516a4e7656d0e4af28af6192bbb8
Cristhian Paucara <ivan.paucara@uab.edu.bo>
2023-03-02 16:59:58 -0400

    creacion tabla

--- reloj.php
+++ reloj.php
@@ -14,4 +14,19 @@
     }
     //en caso de error por sqlite3 revisar este link para corregir
     //https://parzibyte.me/blog/2018/02/07/habilitar-extension-sqlite3-php/
+    $q = <<<sql
+    create table if not exists funcionarios 
+    (id int,
+    nombre text,
+    apellido text,
+    cargo text);
+    sql;
+    $r = $db->exec($q);
+    if($r){
+        echo"<p> La tabla funcionarios fue creada exitosamente</p>";
+    }else{
+        echo"<p>EROOR al crear tabla usuarios</p>";
+    }
+    $db->close();
+
 ?>
